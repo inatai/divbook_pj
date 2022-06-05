7b02dca83960e00d375f14b85c20a94e261b811a
inatai <taitai@prog.com>
2022-06-06 03:08:02 +0900

    book cushion

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -11,25 +11,31 @@ function disp_del(){
 }
 
 function disp_hol(){
-	// 「OK」時の処理開始 ＋ 確認ダイアログの表示
 	if(window.confirm('予約不可にしますか？')){
 		return true;
 	}
-	// 「キャンセル」時の処理開始
 	else{
 		window.alert('キャンセルされました');
-                return false; // 警告ダイアログを表示
+                return false;
 	}
 }
 
 function disp_item_del(){
-	// 「OK」時の処理開始 ＋ 確認ダイアログの表示
 	if(window.confirm('消去しますか？')){
 		return true;
 	}
-	// 「キャンセル」時の処理開始
 	else{
 		window.alert('キャンセルされました');
-                return false; // 警告ダイアログを表示
+                return false;
+	}
+}
+
+function disp_book(){
+	if(window.confirm('予約しますか？')){
+		return true;
+	}
+	else{
+		window.alert('キャンセルされました');
+                return false;
 	}
 }
